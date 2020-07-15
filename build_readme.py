7699d2e75cb186bfdeb05a7a5169467cfe93324d
Your Name <legendofmiracles@protonmail.com>
2020-07-15 23:43:57 +0200

    dunno it kinda works, but i am not yet finished yet

--- build_readme.py
+++ build_readme.py
@@ -1,128 +1,31 @@
-from python_graphql_client import GraphqlClient
-import json
-import pathlib
-import re
+import requests
 import os
+import datetime
 
-root = pathlib.Path(__file__).parent.resolve()
-client = GraphqlClient(endpoint="https://api.github.com/graphql")
+token = os.environ.get("SIMONW_TOKEN", "")
 
-
-TOKEN = os.environ.get("SIMONW_TOKEN", "")
-
-
-def replace_chunk(content, marker, chunk, inline=False):
-    r = re.compile(
-        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
-        re.DOTALL,
-    )
-    if not inline:
-        chunk = "\n{}\n".format(chunk)
-    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
-    return r.sub(chunk, content)
-
-
-def make_query(after_cursor=None):
-    return """
-query {
-  viewer {
-    repositories(first: 100, privacy: PUBLIC, after:AFTER) {
-      pageInfo {
-        hasNextPage
-        endCursor
-      }
-      nodes {
-        name
-        description
-        url
-        releases(last:1) {
-          totalCount
-          nodes {
-            name
-            publishedAt
-            url
-          }
-        }
-      }
-    }
-  }
-}
-""".replace(
-        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
-    )
+response = requests.get('https://api.github.com/users/alx365/repos', auth=('alx365', token))
+dates = {}
+for i in response.json():
+    name = i['name']
+    html_url = i['html_url']
+    print(name)
+    #dates[name] = ''
+    releases_url = i['releases_url'].split("{")[0]
+    print(releases_url)
+    response = requests.get(releases_url, auth=('alx365', token))
+    if len(response.json()) > 0:
+        if response.json()[0]['draft'] != "true" and len(response.json()[0]['assets']) > 0:
+            date = response.json()[0]['assets'][0]['created_at']
+            #name = response.json()[0]['assets'][0]['name']
+            dates[name] = [date, html_url]
+run = 0
+for w in sorted(dates, key=dates.get, reverse=True):
+    run += 1
+    if run < 4:
+        print(w + " " + dates[w][0] + "" + dates[w][1])
 
 
-def fetch_releases(oauth_token):
-    repos = []
-    releases = []
-    repo_names = set()
-    has_next_page = True
-    after_cursor = None
-
-    while has_next_page:
-        data = client.execute(
-            query=make_query(after_cursor),
-            headers={"Authorization": "Bearer {}".format(oauth_token)},
-        )
-        print()
-        print(json.dumps(data, indent=4))
-        print()
-        for repo in data["data"]["viewer"]["repositories"]["nodes"]:
-            if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
-                repos.append(repo)
-                repo_names.add(repo["name"])
-                releases.append(
-                    {
-                        "repo": repo["name"],
-                        "repo_url": repo["url"],
-                        "description": repo["description"],
-                        "release": repo["releases"]["nodes"][0]["name"]
-                        .replace(repo["name"], "")
-                        .strip(),
-                        "published_at": repo["releases"]["nodes"][0][
-                            "publishedAt"
-                        ].split("T")[0],
-                        "url": repo["releases"]["nodes"][0]["url"],
-                    }
-                )
-        has_next_page = data["data"]["viewer"]["repositories"]["pageInfo"][
-            "hasNextPage"
-        ]
-        after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
-    return releases
-
-
-if __name__ == "__main__":
-    readme = root / "README.md"
-    project_releases = root / "releases.md"
-    releases = fetch_releases(TOKEN)
-    releases.sort(key=lambda r: r["published_at"], reverse=True)
-    md = "\n".join(
-        [
-            "* [{repo} {release}]({url}) - {published_at}".format(**release)
-            for release in releases[:5]
-        ]
-    )
-    readme_contents = readme.open().read()
-    rewritten = replace_chunk(readme_contents, "recent_releases", md)
-
-    # Write out full project-releases.md file
-    project_releases_md = "\n".join(
-        [
-            (
-                "* **[{repo}]({repo_url})**: [{release}]({url}) - {published_at}\n"
-                "<br>{description}"
-            ).format(**release)
-            for release in releases
-        ]
-    )
-    project_releases_content = project_releases.open().read()
-    project_releases_content = replace_chunk(
-        project_releases_content, "recent_releases", project_releases_md
-    )
-    project_releases_content = replace_chunk(
-        project_releases_content, "release_count", str(len(releases)), inline=True
-    )
-    project_releases.open("w").write(project_releases_content)
-
-    
+#dates.sort()
+#dates.reverse()
+#print(dates)
