c69926a12ceb29c696040464e711f98afcb92e67
Your Name <legendofmiracles@protonmail.com>
2020-07-16 11:57:12 +0200

    basically works now

---
+++ README_prefab.md
@@ -0,0 +1,15 @@
+# HOW DA HECK DID YOU GET TO MY GITHUB PROFILE?!?!?!
+### ANYWAYS HERE IS A SMALL BIO:
+ I don't like to describe myself, so here is what my friends/family say about me:
+
+ -The New York Times "Not the lightest bulb in the box."
+ 
+ -Donald Trump "And you call me an idiot?!?!??"
+ 
+ -My Mother "He's adopted."
+ 
+ -PewDiePie "0/10, NEXT MEME!"
+ 
+
+///////////////////////////AUTOMATIC CONTENT GOES HERE///////////////////////////
+
--- build_readme.py
+++ build_readme.py
@@ -3,8 +3,11 @@ import os
 import datetime
 
 token = os.environ.get("SIMONW_TOKEN", "")
-
-response = requests.get('https://api.github.com/users/alx365/repos', auth=('alx365', token))
+#print(token)
+response = requests.get('https://api.github.com/users/alx365/repos',
+    auth=('alx365', token)
+    #headers={"Authorization": "token" + token}
+    )
 dates = {}
 for i in response.json():
     name = i['name']
@@ -20,12 +23,22 @@ for i in response.json():
             #name = response.json()[0]['assets'][0]['name']
             dates[name] = [date, html_url]
 run = 0
+injection_text = "### Latest releases:"
 for w in sorted(dates, key=dates.get, reverse=True):
     run += 1
     if run < 4:
-        print(w + " " + dates[w][0] + "" + dates[w][1])
+        print(w + " " + dates[w][0] + " " + dates[w][1])
+        
+        injection_text = injection_text + "\n "+ str(run) + " . [" + w +"]("+dates[w][1]+"): " + dates[w][0]
+        
+        
+#print(injection_text)
+f = open('README_prefab.md')
+text = f.read().replace("///////////////////////////AUTOMATIC CONTENT GOES HERE///////////////////////////", injection_text)
+print("File: " + text)
 
-
+readme = open("README.md", "w+")
+readme.write(text)
 #dates.sort()
 #dates.reverse()
 #print(dates)
