e7dd02fa2b2b10a853e03400356c382f398acbb0
Your Name <legendofmiracles@protonmail.com>
2020-07-30 20:15:39 +0200

    replaced my uname with a variable

--- build_readme.py
+++ build_readme.py
@@ -3,9 +3,11 @@ import os
 import datetime
 
 token = os.environ.get("SIMONW_TOKEN", "")
+user = "alx365"
 #print(token)
-response = requests.get('https://api.github.com/users/alx365/repos',
-    auth=('alx365', token)
+response = requests.get('https://api.github.com/users/'+ user +'/repos',
+    #you can get the token in the github settings. then you have to add them into the repo as a secret and change the secret's name in manual.yml
+    auth=(user, token)
     #headers={"Authorization": "token" + token}
     )
 dates = {}
@@ -16,7 +18,7 @@ for i in response.json():
     #dates[name] = ''
     releases_url = i['releases_url'].split("{")[0]
     print(releases_url)
-    response = requests.get(releases_url, auth=('alx365', token))
+    response = requests.get(releases_url, auth=(user, token))
     if len(response.json()) > 0:
         if response.json()[0]['draft'] != "true" and len(response.json()[0]['assets']) > 0:
             date = response.json()[0]['assets'][0]['created_at']
